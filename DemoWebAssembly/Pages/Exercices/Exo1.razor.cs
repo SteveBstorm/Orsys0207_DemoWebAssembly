b4c3fc4cd10714022d509323799ab35f59fb0a97
stevebstorm <steve.lorent@gmail.Com>
2024-07-02 15:55:11 +0200

    Correctif Exercices 1 (QUIZZ)

---
+++ DemoWebAssembly/Pages/Exercices/Exo1.razor.cs
@@ -0,0 +1,26 @@
+﻿namespace DemoWebAssembly.Pages.Exercices
+{
+    public partial class Exo1
+    {
+        public string Prenom { get; set; }
+
+        public List<string> Reponses { get; set; }
+
+        public bool PartieTerminee { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Reponses = new List<string>();
+        }
+
+        public void EnregisterReponse(string reponse)
+        {
+            Reponses.Add(reponse);
+            if(Reponses.Count > 2)
+            {
+                PartieTerminee = true;
+            }
+        }
+
+    }
+}
---
+++ DemoWebAssembly/Pages/Exercices/Quizz.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DemoWebAssembly.Pages.Exercices
+{
+    public partial class Quizz
+    {
+        [Parameter]
+        public string Prenom { get; set; }
+
+        [Parameter]
+        public EventCallback<string> RepondreEvent { get; set; }
+
+        public List<string> Questions { get; set; }
+        public int Compteur { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Questions = new List<string>();
+            Questions.Add("La sieste a t'elle été agréable ?");
+            Questions.Add("Blazor vous plait il ?");
+            Questions.Add("On continue ?");
+
+            Compteur = 0;
+        }
+
+        public void Repondre(string reponse)
+        {
+            RepondreEvent.InvokeAsync(reponse);
+            Compteur++;
+        }
+    }
+}
