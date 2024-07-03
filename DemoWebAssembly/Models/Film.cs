0c5dda96a95e7432dcf5c057c6083720cc719e11
stevebstorm <steve.lorent@gmail.Com>
2024-07-03 10:31:09 +0200

    Ajout démo formulaire

---
+++ DemoWebAssembly/Models/Film.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoWebAssembly.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Titre { get; set; }
+        [Range(1977, 2024)]
+        public int AnneeSortie { get; set; }
+        [MinLength(10)]
+        public string Synopsis { get; set; }
+        //Microsoft.AspNetCore.Components.DataAnnotations.Validation
+        [ValidateComplexType]
+        public Personne Realisateur { get; set; }
+
+        public string Genre { get; set; }
+
+        public bool Bonfilm { get; set; }
+
+        public Film()
+        {
+            Realisateur = new Personne();
+        }
+    }
+
+    public class Personne
+    {
+        [Required]
+        public string Nom { get; set; }
+        [Required]
+        public string Prenom { get; set; }
+    }
+}
---
+++ DemoWebAssembly/Pages/Demos/Demo6.razor.cs
@@ -0,0 +1,34 @@
+﻿using DemoWebAssembly.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DemoWebAssembly.Pages.Demos
+{
+    public partial class Demo6
+    {
+        /*
+         L'objet qui sera lié au formulaire doit être instancier
+         !!!NE PAS OUBLIER LES OBJETS COMPLEXES / IMBRIQUES !!!
+
+        Microsoft.AspNetCore.Components.DataAnnotations.Validation
+        Pour la validation des types complexes
+         */
+        public Film NouveauFilm { get; set; }
+        public List<string> listeGenre { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NouveauFilm = new Film();
+            listeGenre = new List<string>();
+            listeGenre.Add("Action");
+            listeGenre.Add("Comédie");
+            listeGenre.Add("Sci-fi");
+            listeGenre.Add("Horreur");
+        }
+
+        public void ValidForm()
+        {
+            Console.WriteLine(JsonSerializer.Serialize(NouveauFilm));
+        }
+    }
+}
