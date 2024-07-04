dee1a881fc2c5d838b05f14030aecb919c03a6c3
stevebstorm <steve.lorent@gmail.Com>
2024-07-04 15:12:56 +0200

    Add SignalR Client

---
+++ DemoWebAssembly/Pages/Demos/SignalRChat/Chat.razor.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace DemoWebAssembly.Pages.Demos.SignalRChat
+{
+    public partial class Chat
+    {
+        //Microsoft.AspNetCore.SignalR.Client
+
+        public HubConnection Connexion{ get; set; }
+        public List<string> Messages { get; set; }
+
+        public string NewMessage { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Messages = new List<string>();
+            Connexion = new HubConnectionBuilder().WithUrl("https://localhost:7107/chathub").Build();
+            await Connexion.StartAsync();
+
+            Connexion.On("SendMessage", (string message) =>
+            {
+                Messages.Add(message);
+                StateHasChanged();
+            });
+        }
+
+        public async Task EnvoiMessage()
+        {
+            await Connexion.SendAsync("SendMessage", NewMessage);
+        }
+
+        public async Task JoinGroup()
+        {
+            await Connexion.SendAsync("JoinGroup", "monsupergroupe");
+            Connexion.On("SendToGroup_monsupergroupe", (string message) =>
+            {
+                Messages.Add(message);
+                StateHasChanged();
+            });
+        }
+
+        public async Task SendToGroup()
+        {
+            await Connexion.SendAsync("SendToGroup", "monsupergroupe", NewMessage);
+        }
+    }
+}
